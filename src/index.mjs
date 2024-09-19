4373da7be69f8d728f382549d94b623cade3d04d
Vujadin Dagovic <vujke456@gmail.com>
2024-09-19 20:44:33 +0200

    learning about cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,18 @@
 import express from "express";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser("helloworld"));
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
 //get requests
 app.get("/", (request, response) => {
+  response.cookie("hello", "world", { maxAge: 60000, signed: true });
   response.status(201).send({ message: "Hello" });
 });
 
